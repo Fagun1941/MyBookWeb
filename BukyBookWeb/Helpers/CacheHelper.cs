88f86849eb6d143488afa0457b44a95038ccdfcf
Fagun1941 <fagun.molla@brainstation-23.com>
2025-09-23 12:38:11 +0600

    End of InMemoryCache

--- BukyBookWeb/Helpers/CacheHelper.cs
+++ BukyBookWeb/Helpers/CacheHelper.cs
@@ -1,27 +1,36 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
 
-namespace BukyBookWeb.Helpers
+public static class CacheHelper
 {
-    public static class CacheHelper
+    private static readonly HashSet<string> Keys = new();
+
+    public static T GetOrSet<T>(IMemoryCache cache, string key, Func<T> fetchFunc, TimeSpan? absolute = null, TimeSpan? sliding = null)
     {
-        public static T GetOrSet<T>(IMemoryCache cache, string key, Func<T> fetchFunc, TimeSpan? absolute = null, TimeSpan? sliding = null)
+        if (!cache.TryGetValue(key, out T value))
         {
-            if (!cache.TryGetValue(key, out T value))
-            {
-                value = fetchFunc();
-                var options = new MemoryCacheEntryOptions();
-                if (absolute.HasValue) options.SetAbsoluteExpiration(absolute.Value);
-                if (sliding.HasValue) options.SetSlidingExpiration(sliding.Value);
+            value = fetchFunc();
+            var options = new MemoryCacheEntryOptions();
+            if (absolute.HasValue) options.SetAbsoluteExpiration(absolute.Value);
+            if (sliding.HasValue) options.SetSlidingExpiration(sliding.Value);
 
-                cache.Set(key, value, options);
-            }
+            cache.Set(key, value, options);
 
-            return value;
+            lock (Keys) Keys.Add(key);
         }
 
-        public static void Remove(IMemoryCache cache, string key)
+        return value;
+    }
+
+    public static void Remove(IMemoryCache cache, string prefix)
+    {
+        lock (Keys)
         {
-            cache.Remove(key);
+            var keysToRemove = Keys.Where(k => k.StartsWith(prefix)).ToList();
+            foreach (var key in keysToRemove)
+            {
+                cache.Remove(key);
+                Keys.Remove(key);
+            }
         }
     }
 }
